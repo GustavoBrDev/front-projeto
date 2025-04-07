44a7608fa32d57e021c1a85e1adb8b2376f64662
JoanaVoigt <147541974+JoanaVoigt@users.noreply.github.com>
2025-04-07 19:02:32 -0300

    Tela de Configurações
    A tela de configuration foi alterada e foram criados os componentes "Switch" e "ConfiguracoesPanel"

--- src/app/configuration/page.jsx
+++ src/app/configuration/page.jsx
@@ -1,8 +1,41 @@
-import React from "react";
-import MaintenancePage from "@/components/pagesResponses/Maintenance";
+"use client"
+
+import { useState, useEffect } from "react"
+import { HeaderDemo } from "@/components/Header/HeaderDemo"
+import { Header } from "@/components/Header/Header"
+import { BlueBackground } from "@/components/topBar/BlueBackground"
+import { WhiteContainer } from "@/components/White-Container"
+import { ConfigurationTitle } from "@/components/topBar/ConfigurationTitle"
+import { Loading } from "@/components/Loading"
+import ConfiguracoesPanel from "@/components/ConfiguracoesPanel";
 
 export default function Configuration() {
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() => setIsLoading(false), 1000)
+        return () => clearTimeout(timer)
+      }, [])
     return (
-        <MaintenancePage {...{ isPlanned: true,  estimatedTime: "1 Semana", customMessage: "Essa página está em desenvolvimento" }} />
-    );
-};
+        <div className="min-h-screen bg-gray-100 mx-auto">
+        <Header />
+        
+        <BlueBackground>
+            <ConfigurationTitle iconWidth={40} iconHeight={40} textSize={"3xl"} />
+        </BlueBackground>
+
+        <div className="container flex justify-center w-screen mx-auto">
+            <WhiteContainer>
+                {isLoading ? (
+                    <Loading />
+                ) : (
+                    <div className="space-y-8">
+                        <ConfiguracoesPanel />
+                    </div>
+                )}
+            </WhiteContainer>
+        </div>
+        <HeaderDemo />
+    </div>
+);
+}
